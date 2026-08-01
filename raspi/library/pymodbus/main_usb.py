c6020965aaa0876227c4ed073ed84fa70f07f86d
hanif-page <amrhanif15@gmail.com>
2026-08-01 12:36:51 +0700

    feat: Implement the Data to CSV auto-save mechanism in the pymodbus program.

--- raspi/library/pymodbus/main_usb.py
+++ raspi/library/pymodbus/main_usb.py
@@ -27,6 +27,7 @@ deactivate # deactivate the environment
 from pymodbus.client import ModbusSerialClient
 import time
 from datetime import datetime
+from sensordata import SensorData
 
 def connectToClient(port, baudRate):
     client = ModbusSerialClient(port=port, timeout=2, baudrate=baudRate, bytesize=8, parity="N", stopbits=1)
@@ -39,18 +40,25 @@ def connectToClient(port, baudRate):
         exit(1)
 
 def runMonitorDisplay(client):
-    try:
-        while True:
+    sensor = SensorData(filePathRelativeToDriver="data") # the sensor data class, takes the target file output path as an argument
 
+    try:
+
+        while True:
             strDate = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             print(f"== MONITOR DATA ({strDate}) ==\n")
 
+            # string to be stored in the database
+            _date = datetime.now().strftime('%Y-%m-%d')
+            _time = datetime.now().strftime('%H-%M-%S')
+
             # Read Temperature
             temperature = client.read_input_registers(address=1, count=1, device_id=1)
+            _temperature = temperature.registers[0] / 10.0
             # print(temperature)
             if not temperature.isError():
                 print(f"Temperature (raw data): {temperature.registers[0]}")
-                print(f"Temperature (°C): {temperature.registers[0] / 10.0}") # assume that the raw data scaled by 10x
+                print(f"Temperature (°C): {_temperature}") # assume that the raw data scaled by 10x
             else:
                 print(f"Error reading Temperature: {temperature}")
 
@@ -58,26 +66,44 @@ def runMonitorDisplay(client):
 
             # Read Humidity
             humidity = client.read_input_registers(address=2, count=1, device_id=1)
+            _humidity = humidity.registers[0] / 10.0
             if not humidity.isError():
                 print(f"Humidity (raw data): {humidity.registers[0]}")
-                print(f"Humidity (%RH): {humidity.registers[0] / 10.0}")
+                print(f"Humidity (%RH): {_humidity}")
             else:
                 print(f"Error reading Humidity: {humidity}")
 
             print() # space
 
             deviceAddr = client.read_holding_registers(address=257, count=1, device_id=1)
-            print(f"Device Address: {deviceAddr.registers[0]}")
+            _deviceAddr = deviceAddr.registers[0]
+            print(f"Device Address: {_deviceAddr}")
 
             baudRate = client.read_holding_registers(address=258, count=1, device_id=1)
-            print(f"Baud Rate: {baudRate.registers[0]}")
+            _baudRate = baudRate.registers[0]
+            print(f"Baud Rate: {_baudRate}")
 
             # [STILL WRONG]
             # both of this data still output 0, which I assume for now, it's a false output...?
             temperatureCorrection = client.read_holding_registers(address=259, count=1, device_id=1)
-            print(f"Temperature Correction (Raw Data): {temperatureCorrection.registers[0]}")
+            _temperatureCorrection = temperatureCorrection.registers[0]
+            print(f"Temperature Correction (Raw Data): {_temperatureCorrection}")
             humidityCorrection = client.read_holding_registers(address=260, count=1, device_id=1)
-            print(f"Humidity Correction (Raw Data): {humidityCorrection.registers[0]}")
+            _humidityCorrection = humidityCorrection.registers[0]
+            print(f"Humidity Correction (Raw Data): {_humidityCorrection}")
+
+            # create the sensor data from the SensorData class
+            newData = sensor.createData(
+                date=_date, 
+                time=_time,
+                temperature=_temperature,
+                humidity=_humidity,
+                deviceAddress=_deviceAddr,
+                baudRate=_baudRate,
+                temperatureCorrection=_temperatureCorrection,
+                humidityCorrection=_humidityCorrection
+            )
+            sensor.addData(newData)
 
             # instruction of how to exit from the loop
             print("\n[Press ctrl+C to Exit from the Loop]")
@@ -89,6 +115,8 @@ def runMonitorDisplay(client):
     except Exception as e:
         print(f"Error: {e}")
     finally:
+        sensor.saveDataToCsv()
+
         runOptionDisplay(client)
 
 def changeBaudRate(client, newBaudRate):
