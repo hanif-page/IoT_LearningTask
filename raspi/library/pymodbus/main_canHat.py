643910c8b758b656dc9472ba86d9ce86dfdd10ca
hanif-page <amrhanif15@gmail.com>
2026-08-01 14:59:45 +0700

    feat: Remove the venv and setup comments (moved to the setup.py in the root). And enable the absolute import of sensordata

--- raspi/library/pymodbus/main_canHat.py
+++ raspi/library/pymodbus/main_canHat.py
@@ -1,25 +1,3 @@
-# Virtual Environment: venv
-
-# Why? Because the program only runs in the same Raspi. We are not planned to move the program to another OS. So I think that it's better to use just venv and isolating the project dependencies.
-
-# venv guidelines: https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/
-
-# How to setup .venv (run in the Terminal)
-"""
-python3 -m venv .venv # .venv is the name of the folder!
-
-source .venv/bin/activate # activate the environment!
-
-which python # check if the python comes from the .venv/bin folder!
-
-# install any dependencies
-
-# set all dependencies to the requirements.txt file
-pip freeze > requirements.txt
-
-deactivate # deactivate the environment
-"""
-
 # Great Source
 # https://bisaioti.com/modbus-rtu-raspberry-pi-master-sensor-suhu/
 # https://www.pymodbus.com/
