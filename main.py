a4015b3ab7f4b3a9c798bcc0373b7b39e734ccaf
hanif-page <amrhanif15@gmail.com>
2026-08-23 10:00:07 +0700

    feat: Resolve the while loop and time.sleep problem which cause the pages won't change to the Monitor Display. Finished the control display too. Next: (check gcal)

--- main.py
+++ main.py
@@ -8,29 +8,30 @@
 
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import Qt, QCoreApplication
+from PySide6.QtCore import QCoreApplication, QTimer
 from ui_mainwindow import Ui_MainWindow
 
-from datetime import datetime
-import time
 
 from raspi.library.pymodbus.main_usb import PyModbusModule # importing the class!
 # from raspi.library.minimal_modbus.main_usb 
 # from raspi.no_library import main_usb as pyserial # but this still won't work because I haven't develop it!
 
 class MainWindow(QMainWindow):
-    def __init__(self, modbusClient):
+    def __init__(self, modbusModule, modbusClient):
         super(MainWindow, self).__init__()
 
         """
-        This modbusClient could be Pymodbus, Minimalmodbus, or Pyserial
+        This modbusModule could be Pymodbus, Minimalmodbus, or Pyserial
 
         Requirements: We need to set Pymodbus, Minimalmodbus, and Pyserial with the same function under it. Let:
         - connectToClient()
         - changeBaudRate(client, newBaudRate)
         - getMonitoredData(client) 
         """
+        self.modbusModule = modbusModule
         self.modbusClient = modbusClient
+        self.isMonitoring = False
+        self.isBaudRateControlSuccess = False
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
@@ -41,17 +42,29 @@ class MainWindow(QMainWindow):
         # When monitor_button clicked
         self.ui.monitor_button.clicked.connect(self.runMonitorDisplay)
 
+        self.ui.stopAndSave_button.clicked.connect(self.stopMonitorDisplay)
+
+        # The Data Loop & Delay using QTimer! (explanation: In the IoT Task Notion!!)
+        # https://doc.qt.io/qtforpython-6/PySide6/QtCore/QTimer.html
+        self.monitorTimer = QTimer()
+        self.monitorTimer.setInterval(5000) # 5000ms or 5s delay!
+        self.monitorTimer.timeout.connect(self.stopControlDisplay) # when the 5 second ends, call the recurring function inside the connect parameter!
+
         # When control_button clicked
-        self.ui.control_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.controlDisplay))
+        self.ui.control_button.clicked.connect(self.runControlDisplay)
+
+        # The Delay after one of the baud rate button is clicked
+        self.controlTimer = QTimer()
+        self.controlTimer.setInterval(3000) # 3s delay!
+        self.controlTimer.timeout.connect(self.stopControlDisplay)
 
         # later, create an additional function to run the process
-        self.ui.set9600_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.optionDisplay))
-        self.ui.set14400_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.optionDisplay))
-        self.ui.set19200_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.optionDisplay))
+        self.ui.set9600_button.clicked.connect(lambda: self.setBaudRate(baudRate=9600))
+        self.ui.set14400_button.clicked.connect(lambda: self.setBaudRate(baudRate=14400))
+        self.ui.set19200_button.clicked.connect(lambda: self.setBaudRate(baudRate=19200))
 
         # Displaying the Changed Baud Rate only after one of the button clicked! (Control Display)
-        # self.ui.label_9.setText(QCoreApplication.translate("MainWindow", u"*Baud Rate Changed To: ", None))
-        # self.ui.newBaudRate_value.setText(QCoreApplication.translate("MainWindow", u"9600", None))
+        # In default, it is set to none because we don't want to show the text at first!
         self.ui.label_9.setText(QCoreApplication.translate("MainWindow", u"", None))
         self.ui.newBaudRate_value.setText(QCoreApplication.translate("MainWindow", u"", None))
 
@@ -59,48 +72,80 @@ class MainWindow(QMainWindow):
         self.ui.update_settings_button.clicked.connect(lambda: print(f"Port: {self.ui.port_input.toPlainText()}\nLibrary: {self.ui.library_pick.currentText()}"))
 
     def runMonitorDisplay(self):
-        # changing the stacked pages to the Monitor Page
         self.ui.stackedWidget.setCurrentWidget(self.ui.monitorDisplay)
 
         self.isMonitoring = True
 
-        self.ui.stopAndSave_button.clicked.connect(self.stopMonitorDisplay)
+        # set the first data displayed in the table (before entering the QTimer loop interval)
+        self.updateMonitoringData()
 
-        # Run the loop to continuously change the data!
-        while self.isMonitoring:
-            print("Capturing Data...")
-            ... # the monitoring and save logic
-
-            time.sleep(5) # 5 second interval
-
-        """
-        CURRENT BIG PROBLEM: I haven't figure this bug out, but, the main problem is when I click the monitor button, the Pages won't changed, but the while loop runs!
-        """
-
+        # start the QTimer interval timer! 
+        self.monitorTimer.start()
     def stopMonitorDisplay(self):
         self.isMonitoring = False
         self.ui.stackedWidget.setCurrentWidget(self.ui.optionDisplay)
-        print("Out from the Monitor Display!")
 
+        # stop the QTimer interval timer! 
+        self.monitorTimer.stop()
+
+        print("Out from the Monitor Display!")
+    def updateMonitoringData(self):
+        try:
+            # Get Data
+            data = self.modbusModule.getMonitoredData(self.modbusClient)
+        
+            # change the Monitor Data date and time
+            self.ui.monitor_data_timestamp.setText(QCoreApplication.translate("MainWindow", f"MONITOR DATA ({data['date']} {data['time']})", None))
+
+            # Change/Update the table value data
+            """
+                dictionary template: dict(
+                    date=date,
+                    time=time,
+                    temperature=temperature
+                    humidity=humidity,
+                    deviceAddress=deviceAddress,
+                    baudRate=baudRate,
+                    temperatureCorrection=temperatureCorrection,
+                    humidityCorrection=humidityCorrection
+                )
+            """
+            self.ui.data_table.item(0, 0).setText(f"{data['temperature']}")
+            self.ui.data_table.item(1, 0).setText(f"{data['humidity']}")
+            self.ui.data_table.item(2, 0).setText(f"{data['deviceAddress']}")
+            self.ui.data_table.item(3, 0).setText(f"{data['baudRate']}")
+            self.ui.data_table.item(4, 0).setText(f"{data['temperatureCorrection']}")
+            self.ui.data_table.item(5, 0).setText(f"{data['humidityCorrection']}")
+        except Exception as e:
+            print(f"Error when updating data: {e}")
 
     def runControlDisplay(self):
-        ...
+        self.ui.stackedWidget.setCurrentWidget(self.ui.controlDisplay)
+    def setBaudRate(self, baudRate: int):
+        if self.modbusModule.changeBaudRate(self.modbusClient, baudRate):
+            print(f"Baud Rate successfully changed to {baudRate}")
+            self.isBaudRateControlSuccess = True
 
-# class ModbusClient:
-#     def __init__(self, modbusClientModule, clientType: int = 0):
-#         """
-#         modbusClientModule
+            self.ui.label_9.setText(QCoreApplication.translate("MainWindow", u"*Baud Rate Changed To:", None))
+            self.ui.newBaudRate_value.setText(QCoreApplication.translate("MainWindow", f"{baudRate}", None))
 
-#         clientType
-#             0: Pymodbus (default)
-#             1: Minimalmodbus
-#             2: Pyserial
-#         """
-#         self.modbusClientModule = modbusClientModule
-#         self.clientType = clientType
+            self.controlTimer.start()
+        else:
+            print(f"Baud Rate failed to be changed!")
+            self.isBaudRateControlSuccess = False
+    def stopControlDisplay(self):
+        if self.isBaudRateControlSuccess:
+            self.isBaudRateControlSuccess = False
+            self.ui.stackedWidget.setCurrentWidget(self.ui.optionDisplay)
+            self.controlTimer.stop()
 
-#     def 
-        
+            self.ui.label_9.setText(QCoreApplication.translate("MainWindow", u"", None))
+            self.ui.newBaudRate_value.setText(QCoreApplication.translate("MainWindow", u"", None))
+
+            print("Out from the Control Display!")
+        else:
+            self.ui.stackedWidget.setCurrentWidget(self.ui.optionDisplay)
+            print("Out from the Control Display!")
 
 if __name__ == "__main__":
     # DEFAULT VALUE
@@ -116,11 +161,7 @@ if __name__ == "__main__":
 
     app = QApplication(sys.argv)
 
-    window = MainWindow(pymodbusClient)    
-
-    # strDate = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # this is doable, but I haven't check if this is OK or NOT OK in OOP concept! 
-    # window.ui.monitor_data_timestamp.setText(QCoreApplication.translate("MainWindow", f"MONITOR DATA ({strDate})", None))
+    window = MainWindow(pymodbus, pymodbusClient)    
 
     window.show()
 
